482f41e186f24f9cfd7630875497bea510dd7dd3
Hans Pistor <hpistor@fastmail.com>
2023-04-24 19:06:40 -0700

    Move rendering to lib + basic state
    We want to have "States" to our game so that we can load all our assets
    before we start rendering them.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
-use bevy::prelude::Plugin;
-use registry::RegistryPlugin;
+use crate::registry::RegistryPlugin;
+use crate::render::RenderPlugin;
+use bevy::prelude::*;
 
 pub mod registry;
+pub mod render;
 pub mod voxel;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Default, States)]
+pub enum GameState {
+    #[default]
+    Loading,
+    Running,
+}
 
 pub struct BevyMinecraftPlugin;
 
 impl Plugin for BevyMinecraftPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_plugin(RegistryPlugin);
+        app.add_state::<GameState>()
+            .add_plugin(RegistryPlugin)
+            .add_plugin(RenderPlugin);
     }
 }
